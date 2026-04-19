b6993c41eaf580076c453f7bd714f5d6eb04ce2e
Jake6654 <ugw2ad@virginia.edu>
2026-04-18 20:47:38 -0400

    first commit

---
+++ spring-api/src/main/java/com/logpilot/logpilot/LogpilotApplication.java
@@ -0,0 +1,14 @@
+package com.logpilot.logpilot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// This annotation tells Spring Boot, "this is the starting point of the application"
+@SpringBootApplication
+public class LogpilotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogpilotApplication.class, args);
+	}
+
+}
---
+++ spring-api/src/test/java/com/logpilot/logpilot/LogpilotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logpilot.logpilot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogpilotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
