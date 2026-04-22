063244db3ac93f58e9c76c9c17d60656f9546615
Jake6654 <ugw2ad@virginia.edu>
2026-04-21 20:12:24 -0400

    feat: add log filtering by level using query parameters and service

--- spring-api/src/main/java/com/opslens/controller/HealthController.java
+++ spring-api/src/main/java/com/opslens/controller/HealthController.java
@@ -4,6 +4,7 @@ package com.opslens.controller;
 import com.opslens.model.LogItem;
 import com.opslens.service.LogService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -26,7 +27,10 @@ public class HealthController {
     }
 
     @GetMapping("/logs")
-    public List<LogItem> getLogs(){
-        return logService.getLogs();
+    public List<LogItem> getLogs(
+            // Read level from URL query parameter
+            @RequestParam(required = false) String level
+    ){
+        return logService.getLogs(level);
     }
 }
--- spring-api/src/main/java/com/opslens/service/LogService.java
+++ spring-api/src/main/java/com/opslens/service/LogService.java
@@ -9,13 +9,23 @@ import java.util.List;
 @Service
 public class LogService {
 
-    public List<LogItem> getLogs(){
-        return List.of(
-                // return multiple log items grouped by a list
-                // Spring automatically converts it to JSON
+    public List<LogItem> getLogs(String level){
+
+        // Get all logs
+        List<LogItem> logs = List.of(
                 new LogItem("2026-04-18T13:00:00", "INFO", "spring-api", "User login success"),
                 new LogItem("2026-04-18T13:01:10", "WARN", "spring-api", "Slow response detected"),
                 new LogItem("2026-04-18T13:02:25", "ERROR", "spring-api", "Database timeout")
         );
+
+        if (level == null){
+            return logs;
+        }
+
+        // filtering logic
+        return logs.stream()
+                .filter(log -> log.getLevel().equalsIgnoreCase(level))
+                .toList();
+
     }
 }
